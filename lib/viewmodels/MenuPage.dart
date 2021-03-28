0104495b3572b37c4a2c0b4463946b587fdf4d08
DenXman111 <DenXman123@gmail.com>
2021-03-29 00:41:05 +0200

    Added main menu

---
+++ lib/viewmodels/MenuPage.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../views/MenuPageState.dart';
+
+class MenuPage extends StatefulWidget {
+  static String routeName = "/";
+
+  @override
+  State<StatefulWidget> createState() => MenuPageState();
+}
---
+++ lib/views/MenuPageState.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../viewmodels/MenuPage.dart';
+
+class MenuPageState extends State<MenuPage>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'English learning',
+          style: GoogleFonts.quicksand(
+            fontSize: 32,
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+        actions: <Widget>[
+          RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(text: '12 ', style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.w300)),
+                WidgetSpan(child: Icon(Icons.stars_rounded, size: 32)),
+              ],
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextButton(
+              child: Text(
+                'Game 1',
+                style: GoogleFonts.quicksand(
+                  fontSize: 48,
+                  fontWeight: FontWeight.w400,
+                ),
+
+              ),
+              style: TextButton.styleFrom(
+                primary: Colors.deepOrange,
+                padding: EdgeInsets.all(16.0),
+                //backgroundColor: Colors.grey,
+                //onSurface: Colors.white,
+              ),
+              onPressed: (){
+                print ('Game 1');
+              },
+            ),
+            SizedBox(height: 25),
+            TextButton(
+              child: Text(
+                'Game 2',
+                style: GoogleFonts.quicksand(
+                  fontSize: 48,
+                  fontWeight: FontWeight.w400,
+                ),
+
+              ),
+              style: TextButton.styleFrom(
+                primary: Colors.deepOrange,
+                //backgroundColor: Colors.grey,
+                //onSurface: Colors.white,
+              ),
+              onPressed: (){
+                print ('Game 2');
+              },
+            ),
+            SizedBox(height: 25),
+            TextButton(
+              child: Text(
+                'Settings',
+                style: GoogleFonts.quicksand(
+                  fontSize: 48,
+                  fontWeight: FontWeight.w400,
+                ),
+
+              ),
+              style: TextButton.styleFrom(
+                primary: Colors.deepOrange,
+                //backgroundColor: Colors.grey,
+                //onSurface: Colors.white,
+              ),
+              onPressed: (){
+                print ('Settings');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

694ec7327b5258df15a4e2bb238cf11d3e60d2a8
Karolina Gontarek <karolina.gontarek20@gmail.com>
2021-04-06 11:20:29 +0200

    Add DB to main menu

---
+++ lib/db/SetUpData.dart
@@ -0,0 +1,24 @@
+const List<Map<String, dynamic>> setUpData = [
+  {
+    "id": 1,
+    "title": "Game 1 from DB",
+    "storageType": "API",
+    "description": "Game one - words translation",
+    "exerciseKeys": []
+  },
+  {
+    "id": 2,
+    "title": "Game 2 from DB",
+    "storageType": "API",
+    "description": "Game two - something",
+    "exerciseKeys": []
+  },
+  {
+    "id": 3,
+    "title": "Game 3 from DB",
+    "storageType": "API",
+    "description": "Game three - grammar",
+    "exerciseKeys": []
+  }
+
+];

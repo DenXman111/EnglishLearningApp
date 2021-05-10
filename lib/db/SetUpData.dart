cdc9781a294b88dd78272b33efa773ee5808f977
DenXman111 <DenXman123@gmail.com>
2021-05-10 02:34:33 +0200

    Database updated

--- lib/db/SetUpData.dart
+++ lib/db/SetUpData.dart
@@ -21,14 +21,28 @@ const List<Map<String, dynamic>> gamesSetUpData = [
     "exerciseKeys": []
   },
   {
+    "id": 4,
+    "title": "Choose word from definition",
+    "storageType": "API",
+    "description": "Description",
+    "exerciseKeys": []
+  },
+  {
     "id": 5,
     "title": "Gap filling",
     "storageType": "API",
     "description": "Game five - grammar",
     "exerciseKeys": []
   },
+  {
+    "id": 6,
+    "title": "Past Participle",
+    "storageType": "API",
+    "description": "Game three - grammar",
+    "exerciseKeys": []
+  },
 ];
 
 const Map<String, dynamic> specialValsSetUpData = {
-    "points_sum": 77
+  "points_sum": 77
 };
